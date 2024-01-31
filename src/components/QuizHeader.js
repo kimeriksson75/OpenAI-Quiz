66c9e16dcbbd68f941539e5eeecff14f228269f7
kimeriksson75 <kimeriksson75@gmail.com>
2024-01-31 20:15:34 +0100

    Buttons texts

--- src/components/QuizHeader.js
+++ src/components/QuizHeader.js
@@ -13,8 +13,8 @@ const QuizHeader = ({ onInitQuiz }) => {
 		<header className='chat__mainHeader'>
 			<p>Surra på eller ta en quiz</p>
 			<div className="button-group">
-				<button className='button-primary' onClick={onInitQuiz}>Börja quizza</button>
-				<button className='button-secondary' onClick={handleLeaveChat}>Hoppa ut</button>
+				<button className='button-primary' onClick={onInitQuiz}>Quizza</button>
+				<button className='button-secondary' onClick={handleLeaveChat}>Hejdå!</button>
 			</div>
 		</header>
 	)
